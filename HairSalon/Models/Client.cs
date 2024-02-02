0030c1f7edc1b63bdc0a851bf6cdfac489534752
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-02 11:27:17 -0800

    Add Client and link with Stylist

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    public string ClientDetails { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist{ get; set; }
+  }
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
   public class Stylist
@@ -5,5 +7,6 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string StylistName { get; set; }
     public string StylistDetails { get; set; }
+    public List<Client> Clients { get; set; }
   }
 }
