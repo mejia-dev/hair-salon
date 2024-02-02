977eae4b5d6d1cbc8c2090fd2f77123fb684a17d
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-02 09:03:21 -0800

    Add Stylist

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,9 @@
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public string StylistDetails { get; set; }
+  }
+}
