67357236cc60ccb95ffa01ebdaec1353b69184fa
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-02 11:58:49 -0800

    Add Client Create action and view with error handling

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,5 +1,6 @@
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,26 @@ namespace HairSalon.Controllers
     {
       ViewBag.PageTitle = "Clients";
       return View(_db.Clients.Include(client => client.Stylist).ToList());
+    }
+
+    public ActionResult Create(string error)
+    {
+      ViewBag.PageTitle = "Add a Client";
+      ViewBag.ErrorMessage = error;
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create", new { error = "noStylists" });
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
